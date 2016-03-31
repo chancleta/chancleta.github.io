6b8d474958add930ad56b5854cb4af0620ba3a26
unknown <lupena@SF2612.domain.synergiesservices.com>
2016-03-31 15:39:52 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chancletagithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chancletagithubioApp
+ */
+angular.module('chancletagithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chancletagithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chancletagithubioApp
+ */
+angular.module('chancletagithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
